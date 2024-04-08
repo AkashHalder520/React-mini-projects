cda8ea0778ee212673e6e6545e392bb80dd6de72
AkashHalder520 <akashhalder520@gmail.com>
2024-04-09 00:46:59 +0530

    Basic pagination done

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from 'react'
 import './Pagination.scss'
 function Pagination() {
-    const [products, setProducts] = useState({})
+    const [products, setProducts] = useState([])
+    const [currentPage, setCurrentPage] = useState(1);
     const fetchProduct = async () => {
         await fetch('https://dummyjson.com/products?limit=100')
             .then(res => res.json()).then(resdata => setProducts(resdata?.products))
@@ -11,41 +12,59 @@ function Pagination() {
     useEffect(() => {
         fetchProduct();
     }, []);
-    const numberOfPages=products.length/10;
-
-   function renderProducts(){
-    // we will render 10 products per page
-//per page products
- products.slice
-
-    console.log(products,"asdfsadf");
-    return( 
-        <>
-        {
-           products.map((item,key)=>{
-            return(
-                <span key={key} >
-                    <img  src={item.thumbnail} alt="" />
-                    <h2>{item.title}</h2>
-                </span>
-            )
-           
-        })
+    const numberOfPages = products.length / 10;
+    function pagenNumber() {
+        const pageNumbers = [];
+        for (let index = 1; index <= numberOfPages; index++) {
+            pageNumbers.push(index);
         }
-        </>
-    )
-   }
+        return pageNumbers
+    }
+    function handeClick(event) {
+        event.preventDefault()
+        let value = event.target.value
+        console.log(value);
+        setCurrentPage(value)
+
+    }
+    function renderProducts() {
+        // we will render 10 products per page
+        //per page products
+        const start = (currentPage - 1) * 10;
+        const end = start + 10;
+        return products.slice(start, end).map((item, index) => (
+            <div key={index}>
+                <img src={item.thumbnail} alt="" />
+                <h2>{item.title}</h2>
+            </div>
+        ));
+       
+    }
+
+
     return (
-       <>
-       <div className='renderproducts'>
-        {products.length>0 ? renderProducts():'' 
-        }
-       </div>
-       <div className='renderPageno'>
-        
+        <>
+            <div className='renderproducts'>
+                {products.length > 0 ? renderProducts() : 'loading......'
+                }
+            </div>
+            <div className='renderPageno'>
+                {
+                    pagenNumber().map((i, key) => {
+                        return (
+                            <button
+                                className='paginationBtn'
+                                key={key}
+                                onClick={handeClick}
+                                value={i}
+                            >{i}</button>
+                        )
+                    })
+                }
 
-       </div>
-       </>
+
+            </div>
+        </>
     )
 }
 
