ad09beeead2a65d59ddc4198321535242a2761a5
AkashHalder520 <akashhalder520@gmail.com>
2024-04-08 19:27:35 +0530

    pagination to be continued

--- src/components/Pagination/Pagination.jsx
+++ src/components/Pagination/Pagination.jsx
@@ -1,21 +1,49 @@
 import { useEffect, useState } from 'react'
-
+import './Pagination.scss'
 function Pagination() {
     const [products, setProducts] = useState({})
     const fetchProduct = async () => {
         await fetch('https://dummyjson.com/products?limit=100')
-            .then(res => res.json()).then(resdata => setProducts(resdata))
+            .then(res => res.json()).then(resdata => setProducts(resdata?.products))
     }
     console.log(products);
     // Call the function to fetch data (optional)
     useEffect(() => {
         fetchProduct();
     }, []);
-   
+    const numberOfPages=products.length/10;
+
+   function renderProducts(){
+    // we will render 10 products per page
+//per page products
+ products.slice
+
+    console.log(products,"asdfsadf");
+    return( 
+        <>
+        {
+           products.map((item,key)=>{
+            return(
+                <span key={key} >
+                    <img  src={item.thumbnail} alt="" />
+                    <h2>{item.title}</h2>
+                </span>
+            )
+           
+        })
+        }
+        </>
+    )
+   }
     return (
        <>
-       <div>
+       <div className='renderproducts'>
+        {products.length>0 ? renderProducts():'' 
+        }
+       </div>
+       <div className='renderPageno'>
         
+
        </div>
        </>
     )
